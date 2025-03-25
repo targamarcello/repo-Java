462d97f0f4ab140e840a5e6e47c4bfb96c2972fd
Marcello Targa <121225428+targamarcello@users.noreply.github.com>
2025-03-25 12:24:35 +0100

    Add files via upload

---
+++ Marzo/SoftwareScuole/src/FrontEnd/Tools.java
@@ -0,0 +1,90 @@
+// Tools.java
+package FrontEnd;
+
+import Negozio.Prodotti.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Tools {
+
+    private Tools() {
+    } // Prevents instantiation of the class
+
+    public static void clrScr() {
+        try {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void Wait(int attesa) {
+        try {
+            Thread.sleep(attesa);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int Menu(String opzioni[], Scanner tastiera) {
+        int scelta;
+
+        do {
+            clrScr();
+            System.out.println("------------------");
+            System.out.println(opzioni[0]);
+            System.out.println("------------------");
+            for (int i = 1; i < opzioni.length; i++) {
+                System.out.println("[" + i + "]" + " " + opzioni[i]);
+            }
+            scelta = Integer.parseInt(tastiera.nextLine());
+            if ((scelta < 1) || (scelta > opzioni.length - 1)) {
+                System.out.println("Opzione Sbagliata");
+                Wait(2000);
+            }
+        } while ((scelta < 1) || (scelta > opzioni.length - 1));
+        return scelta;
+    }
+
+    public static Smartphone leggiSmartphone(Scanner scanner) {
+        System.out.println("Inserire codice:");
+        String codice = scanner.nextLine();
+        System.out.println("Inserire marca:");
+        String marca = scanner.nextLine();
+        System.out.println("Inserire prezzo:");
+        double prezzo = Double.parseDouble(scanner.nextLine());
+        System.out.println("Inserire modello:");
+        String modello = scanner.nextLine();
+        System.out.println("Inserire memoria:");
+        int memoria = Integer.parseInt(scanner.nextLine());
+        return new Smartphone(prezzo, marca, codice, modello, memoria);
+    }
+
+    public static Prodotto leggiManuale(Scanner scanner) {
+        System.out.println("Inserisci nome manuale:");
+        String nome = scanner.nextLine();
+        System.out.println("Inserisci descrizione manuale:");
+        String desc = scanner.nextLine();
+        System.out.println("Inserisci autore manuale:");
+        String aut = scanner.nextLine();
+        System.out.println("Inserisci ISBN:");
+        String isbn = scanner.nextLine();
+        System.out.println("Inserisci prezzo manuale:");
+        double prez = Double.parseDouble(scanner.nextLine());
+        System.out.println("Inserisci quantità:");
+        int quant = Integer.parseInt(scanner.nextLine());
+        System.out.println("Inserisci argomento manuale:");
+        String argomento = scanner.nextLine();
+        Manuale manuale = new Manuale(nome, desc, aut, isbn, prez, quant, argomento);
+        return manuale;
+    }
+
+    public static void visualizzaProdotti(ArrayList<Prodotto> inventario) {
+        for (Prodotto p : inventario) {
+            System.out.println(p.toString());
+        }
+    }
+
+
+}
---
+++ Marzo/SoftwareScuole/src/Test.java
@@ -0,0 +1,17 @@
+public class Test {
+    public static void main(String[] args) {
+
+    }
+}
+/*Fare un software in Java per i contributi statali dovuti alle scuole:
+* le scuole sono: elementari - medie - superiori
+* ogni scuola ha il suo codice alfanumerico/denominazione/indirizzo/città/numero studenti/numero classi/numero sedi aggiuntive/numero laboratori
+* le scuole elementari hanno un contributo annuale: gli studenti pagano 125€ e 9000€ per ogni sede aggiuntiva(questi valori in futuro potrebbero ricevere modifiche)
+* le scuole medie hanno contributo annuale: 150€ per studente, 1100€ per ogni laboratorio e 9000€ per ogni sede aggiuntiva(questi valori in futuro potrebbero ricevere modifiche)
+* le scuole superiori sono di tre tipi diversi: licei, tecnici e professionali
+* i licei hanno contributi uguali a quelli delle scuole medie, eccetto per le sedi aggiuntive
+* i tecnici hanno contributo annuale: 3500€ per ogni classe, 6000€ per ogni laboratorio (questi valori in futuro potrebbero ricevere modifiche)
+* i professionali -che hanno diritto a contributi regionali- hanno contributi statali di 2400€ per ogni classe e di 3000€ per ogni laboratorio (questi valori in futuro potrebbero ricevere modifiche)
+*
+* Bisogna realizzare un programma UML delle classi che rappresentano l'attuale situazione, nel main bisogna istanziare oggetti corrispondenti alle varie tipologie di scuole calcolando e
+* visualizzando per ognuna una sintetica descrizione e il contributo da pagare.*/
